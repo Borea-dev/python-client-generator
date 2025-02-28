3a19a155f56d779f7446653e6adab047c1326ba6
David Bauch <jamesbauch@gmail.com>
2025-02-28 14:56:05 -0700

    refactored borea-python cli into: cli.py

--- src/borea_python/generator.py
+++ src/borea_python/generator.py
@@ -1,11 +1,10 @@
 import json
 from pathlib import Path
-from typing import Any, Dict, List, Optional, Tuple, Union
+from typing import Any, Dict, List, Tuple, Union
 
 import click
 from jinja2 import Environment, FileSystemLoader
 
-from .config_parser import ConfigParser
 from .content_loader import ContentLoader
 from .file_writer import ConfigurableFileWriter
 from .generate_method_metadata import GenerateMethodMetadata
@@ -25,7 +24,6 @@ from .models.sdk_class_models import (
     SdkClassPyJinja,
 )
 from .models.tag_class_models import OperationMetadata, TagClassPyJinja
-from .openapi_parser import OpenAPIParser
 from .x_code_sample_generator import XCodeSampleGenerator
 
 
@@ -389,99 +387,3 @@ class SDKGenerator:
             )
         # Write OpenAPI content to file
         self.file_writer.write(str(openapi_file), json.dumps(openapi_content, indent=2))
-
-
-@click.command()
-@click.option(
-    "--openapi-input",
-    "-i",
-    help="Path to OpenAPI specification file or URL",
-    type=str,
-)
-@click.option(
-    "--sdk-output",
-    "-o",
-    help="Output directory for the generated SDK",
-    type=str,
-)
-@click.option(
-    "--models-output",
-    "-m",
-    help="Output directory for the generated models",
-    type=str,
-)
-@click.option(
-    "--tests",
-    "-t",
-    help="Generate tests",
-    default=None,
-)
-@click.option(
-    "--x-code-samples",
-    "-x",
-    help="Generate x-code-samples",
-    default=None,
-)
-@click.option(
-    "--config",
-    "-c",
-    help="Path to borea.config.json",
-    type=str,
-)
-def main(
-    openapi_input: Optional[str],
-    sdk_output: Optional[str],
-    models_output: Optional[str],
-    tests: Optional[bool],
-    x_code_samples: Optional[bool],
-    config: Optional[str],
-):
-    """Generate a Python SDK from an OpenAPI specification.
-
-    The OpenAPI specification can be provided as a local file path or a URL.
-    For URLs, both JSON and YAML formats are supported.
-    """
-    # Default values
-    default_config = "borea.config.json"
-    default_input = "openapi.json"
-    # default_sdk_output is defined below
-    default_models_dir = "models"
-    default_tests = False
-    default_x_code_samples = False
-
-    # Load borea config
-    borea_config: BoreaConfig = ConfigParser.from_source(config, default_config)
-
-    # Use defaults if CLI args OR config values are not provided
-    openapi_input = openapi_input or borea_config.input.openapi or default_input
-    parser = OpenAPIParser(openapi_input)
-    metadata = parser.parse()
-
-    default_sdk_output = Helpers.clean_file_name(metadata.info.title)
-    sdk_output_path = Path(
-        sdk_output or borea_config.output.clientSDK or default_sdk_output
-    )
-    models_output_path = Path(
-        sdk_output_path
-        / (models_output or borea_config.output.models or default_models_dir)
-    )
-    tests = tests or borea_config.output.tests or default_tests
-    x_code_samples = (
-        x_code_samples or borea_config.output.xCodeSamples or default_x_code_samples
-    )
-
-    generator = SDKGenerator(
-        metadata=metadata,
-        output_dir=sdk_output_path,
-        models_dir=models_output_path,
-        generate_tests=tests,
-        generate_x_code_samples=x_code_samples,
-        borea_config=borea_config,
-    )
-    generator.generate()
-
-    click.echo(f"Successfully generated SDK in: {sdk_output_path}")
-
-
-if __name__ == "__main__":
-    main()
